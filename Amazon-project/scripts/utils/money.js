1a5acd3460d0371564a9901ca32eeee69c3ddcd7
Shubhamraj R Prasad <Shubhamraj4113@gmail.com>
2025-04-05 07:44:30 +0530

    Add files via upload

---
+++ Amazon-project/scripts/utils/money.js
@@ -0,0 +1,5 @@
+export function formatCurrency(priceCents) {
+  return (Math.round(priceCents) / 100).toFixed(2);
+}
+
+export default formatCurrency; // each file can have only one default - export
